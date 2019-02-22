46b0475a908be4f025d9d80769644479c84ef466
nilivi <ninoska94lidia@gmail.com>
2019-02-22 08:52:46 -0500

    Agregando HTML y CSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 /* Acá va tu código */
+document.getElementById("pagina2").style.display = "none";
+document.getElementById("pagina3").style.display = "none";
+
+function hb() {
+    document.getElementById("root").style.display = "none";
+    document.getElementById("pagina2").style.display = "block";
+}
+ function continuar() {
+    document.getElementById("root").style.display = "none";
+    document.getElementById("pagina2").style.display = "none";
+    document.getElementById("pagina3").style.display = "block";
+ }
+ function regresar() {
+    document.getElementById("root").style.display = "block";
+    document.getElementById("pagina2").style.display = "none";
+    document.getElementById("pagina3").style.display = "none";
+ }
+ function cerrar() {
+    window.close();
+  }
